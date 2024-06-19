1ec36745a8650a0b2284675031541e09fc850a10
Tesyaf <abraralif3@gmail.com>
2024-06-19 21:27:27 +0700

    Membuat Class Membuat Window

--- Main.py
+++ Main.py
@@ -5,3 +5,14 @@ import tkinter as tk
 import csv
 from tkinter import filedialog
 
+class ArsipSuratApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Arsip Surat")
+        self.root.geometry("1080x720")
+        self.root.configure(bg='#102C57')
+
+        self.btnColor = "#196e78"
+        self.editing_item = None  # Track the item being edited
+        self.setup_ui()
+        self.load_surat()
