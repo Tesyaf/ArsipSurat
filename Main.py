a46f7f6859c61fdc110c51fd6edef1689a5c009b
Tesyaf <abraralif3@gmail.com>
2024-06-19 21:31:05 +0700

    membuat label untuk menerima kode

--- Main.py
+++ Main.py
@@ -13,6 +13,23 @@ class ArsipSuratApp:
         self.root.configure(bg='#102C57')
 
         self.btnColor = "#196e78"
-        self.editing_item = None  # Track the item being edited
+        self.editing_item = None
         self.setup_ui()
         self.load_surat()
+        
+    def setup_ui(self):
+        # Labels
+        label = Label(self.root, text="Arsip Surat", fg="white",bg='#102C57', font=("Times New Roman", 27))
+        label.grid(row=0, column=0, columnspan=8, rowspan=2, padx=50, pady=40)
+
+        nomorsuratLabel = Label(self.root, text="No Surat",fg="white",bg='#102C57', font=("Times New Roman", 12))
+        perihalLabel = Label(self.root, text="Perihal",fg="white",bg='#102C57', font=("Times New Roman", 12))
+        tglLabel = Label(self.root, text="Tanggal Surat",fg="white",bg='#102C57', font=("Times New Roman", 12))
+        pengirimLabel = Label(self.root, text="Pengirim",fg="white",bg='#102C57', font=("Times New Roman", 12))
+        penerimaLabel = Label(self.root, text="Penerima",fg="white",bg='#102C57', font=("Times New Roman", 12))
+
+        nomorsuratLabel.grid(row=3, column=0, padx=50, pady=5)
+        perihalLabel.grid(row=4, column=0, padx=50, pady=5)
+        tglLabel.grid(row=5, column=0, padx=50, pady=5)
+        pengirimLabel.grid(row=6, column=0, padx=50, pady=5)
+        penerimaLabel.grid(row=7, column=0, padx=50, pady=5)
