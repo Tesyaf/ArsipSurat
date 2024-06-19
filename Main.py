44d4704125f65549450cf7d4a79048a3d4ebc657
zakytaufiq <zakytaufiq24@gmail.com>
2024-06-19 15:17:26 +0700

    import library

--- Main.py
+++ Main.py
@@ -1,13 +1,7 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 import tkinter as tk
+import csv
+from tkinter import filedialog
 
-# Create the main window
-root = tk.Tk()
-root.title("Basic Window")
-root.geometry("300x200")
-
-# Create a label widget
-label = tk.Label(root, text="Hello, Tkinter!")
-label.pack(pady=20)
-
-# Run the application
-root.mainloop()
